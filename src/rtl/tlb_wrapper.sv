e000e72b2afa670776ed7b7ddac2bb4ca28d66f8
Arnau Bigas <arnau.bigas@bsc.es>
2023-11-03 12:11:20 +0100

    Add LICENSE, CONTRIBUTORS and update headers

--- src/rtl/tlb_wrapper.sv
+++ src/rtl/tlb_wrapper.sv
@@ -1,19 +1,21 @@
-/* -----------------------------------------------
- * Project Name   : OpenPiton + Lagarto
- * File           : tlb_wrapper.sv
- * Organization   : Barcelona Supercomputing Center
- * Author(s)      : Neiel I. Leyva Santes
- *                : Xavier Carril Gil 
- *                : Javier Salamero Sanz 
- * Email(s)       : neiel.leyva@bsc.es
- *                : xavier.carril@bsc.es
- *                : javier.salamero@bsc.es
- * References     : 
- * -----------------------------------------------
- * Revision History
- *  Revision   | Author    | Commit | Description
- *  ******     | Neiel L.  |        | 
- * -----------------------------------------------
+/*
+ * Copyright 2023 BSC*
+ * *Barcelona Supercomputing Center (BSC)
+ * 
+ * SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
+ * 
+ * Licensed under the Solderpad Hardware License v 2.1 (the “License”); you
+ * may not use this file except in compliance with the License, or, at your
+ * option, the Apache License version 2.0. You may obtain a copy of the
+ * License at
+ * 
+ * https://solderpad.org/licenses/SHL-2.1/
+ * 
+ * Unless required by applicable law or agreed to in writing, any work
+ * distributed under the License is distributed on an “AS IS” BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
  */
 
 module tlb_wrapper 
